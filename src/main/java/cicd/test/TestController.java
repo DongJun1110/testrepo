a839ac0db59ab6465b4971d8d73ba5fd751768d5
kimdongjun <r2222911r@ajou.ac.kr>
2024-06-24 17:23:57 +0900

    first commit

---
+++ src/main/java/cicd/test/TestController.java
@@ -0,0 +1,17 @@
+package cicd.test;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("")
+    public String test() {
+        return "hello world";
+    }
+}
